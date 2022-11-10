9954ddf6d140ecacff4505ca69765458d2b26e62
AleGuevara99 <aleguevara.yagm@gmail.com>
2022-11-09 21:44:09 -0600

    Card payments screen

--- guanacos_app/lib/src/pages/client/payments/create/client_payments_create_controller.dart
+++ guanacos_app/lib/src/pages/client/payments/create/client_payments_create_controller.dart
@@ -1,59 +1,22 @@
-import 'dart:math';
-
-import 'package:fluttertoast/fluttertoast.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_credit_card/credit_card_model.dart';
 import 'package:get/get.dart';
-import 'package:get_storage/get_storage.dart';
-import 'package:guanacos_app/src/models/address.dart';
-import 'package:guanacos_app/src/models/order.dart';
-import 'package:guanacos_app/src/models/product.dart';
-import 'package:guanacos_app/src/models/response_api.dart';
-import 'package:guanacos_app/src/models/user.dart';
-import 'package:guanacos_app/src/pages/client/orders/create/client_orders_create_controller.dart';
-import 'package:guanacos_app/src/pages/client/products/list/client_products_list_controller.dart';
-import 'package:guanacos_app/src/providers/orders_provider.dart';
 
-class ClientPaymentsCreateController extends GetxController {
-  User user = User.fromJson(GetStorage().read('user') ?? {});
-  
-  OrdersProvider ordersProvider = OrdersProvider();
-  ClientProductsListController productsListController = Get.find();
-  ClientOrdersCreateController clientOrdersCreateController = Get.find();
 
-  void createOrder() async {
-    Address a = GetStorage().read('address') is Address
-        ? GetStorage().read('address')
-        : Address.fromJson(GetStorage().read('address') ?? {});
-    List<Product> products = GetStorage().read('shoppinBag');
-    double totalC = 0.0;
+class  ClientPaymentsCreateController extends GetxController{
 
-    for (var product in products) {
-      totalC = roundDouble(totalC + (product.quantity! * product.price!), 2);
-    }
+  var cardNumber = ''.obs;
+  var expireDate = ''.obs;
+  var cardHolderName = ''.obs;
+  var cvvCode = ''.obs;
+  var isCvvFocused = false.obs;
+  GlobalKey<FormState> keyForm=GlobalKey();
 
-    Order order = Order(
-        idclient: user.id,
-        idaddress: a.id,
-        status: 'PAGADO',
-        total: totalC,
-        detail: products);
-
-    ResponseApi responseApi = await ordersProvider.create(order);
-
-    Fluttertoast.showToast(
-        msg: responseApi.message ?? '', toastLength: Toast.LENGTH_LONG);
-
-    if (responseApi.success == true) {
-      GetStorage().remove('shoppinBag');
-      productsListController.items.value = 0;
-      productsListController.lstProducts = [];
-      clientOrdersCreateController.lstProducts = [];
-      Get.toNamed('/client/home');
-      update();
-    }
-  }
-
-  double roundDouble(double value, int places) {
-    num mod = pow(10.0, places);
-    return ((value * mod).round().toDouble() / mod);
+  void onCreditCardModelChanged(CreditCardModel creditCardModel){
+    cardNumber.value = creditCardModel.cardNumber;
+    expireDate.value = creditCardModel.expiryDate;
+    cardHolderName.value = creditCardModel.cardHolderName;
+    cvvCode.value = creditCardModel.cvvCode;
+    isCvvFocused.value = creditCardModel.isCvvFocused;
   }
 }
--- guanacos_app/lib/src/pages/client/payments/create/client_payments_create_page.dart
+++ guanacos_app/lib/src/pages/client/payments/create/client_payments_create_page.dart
@@ -1,45 +1,99 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_credit_card/credit_card_form.dart';
+import 'package:flutter_credit_card/credit_card_model.dart';
+import 'package:flutter_credit_card/credit_card_widget.dart';
 import 'package:get/get.dart';
 import 'package:guanacos_app/src/pages/client/payments/create/client_payments_create_controller.dart';
 
 // ignore: must_be_immutable
 class ClientPaymentsCreatePage extends StatelessWidget {
-  ClientPaymentsCreatePage({Key? key}) : super(key: key);
-  ClientPaymentsCreateController paymentController = Get.put(ClientPaymentsCreateController());
+
+  ClientPaymentsCreateController con = Get.put(ClientPaymentsCreateController());
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return Obx(() => Scaffold(
       bottomNavigationBar: _buttonNext(context),
-      appBar: AppBar(
-        iconTheme: const IconThemeData(color: Colors.black),
-        title:const Text(
-          'Formas de pago',
+        body: ListView(
+            children: [
+              CreditCardWidget(
+                cardNumber: con.cardNumber.value,
+                expiryDate: con.expireDate.value,
+                cardHolderName: con.cardHolderName.value,
+                cvvCode: con.cvvCode.value,
+                showBackView: con.isCvvFocused.value,
+                cardBgColor: Colors.black54,
+                obscureCardNumber: true,
+                obscureCardCvv: true,
+                height: 175,
+                labelCardHolder: 'NOMBRE Y APELLIDO',
+                textStyle: TextStyle(color: Colors.white),
+                width: MediaQuery.of(context).size.width,
+                animationDuration: Duration(milliseconds: 1000), 
+                onCreditCardWidgetChange: (CreditCardBrand ) {  },
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: 20),
+                child: CreditCardForm(
+                  formKey: con.keyForm, // Required
+                  onCreditCardModelChange: con.onCreditCardModelChanged, // Required
+                  themeColor: Colors.red,
+                  obscureCvv: true,
+                  obscureNumber: true,
+                  cardNumberValidator: (String? cardNumber){},
+                  expiryDateValidator: (String? expiryDate){},
+                  cvvValidator: (String? cvv){},
+                  cardHolderValidator: (String? cardHolderName){},
+                  onFormComplete: () {
+                    // callback to execute at the end of filling card data
+                  },
+                  cardNumberDecoration: const InputDecoration(
+                    suffixIcon: Icon(Icons.credit_card) ,
+                    labelText: 'Numero de la tarjeta',
+                    hintText: 'XXXX XXXX XXXX XXXX',
+                  ),
+                  expiryDateDecoration: const InputDecoration(
+                    suffixIcon: Icon(Icons.date_range),
+                    labelText: 'Expiracion',
+                    hintText: 'XX/XX',
+                  ),
+                  cvvCodeDecoration: const InputDecoration(
+                    suffixIcon: Icon(Icons.lock),
+                    labelText: 'CVV',
+                    hintText: 'XXX',
+                  ),
+                  cardHolderDecoration: const InputDecoration(
+                    suffixIcon: Icon(Icons.person),
+                    labelText: 'Titular de la tarjeta',
+                  ),
+                  cvvCode: '',
+                  expiryDate:'',
+                  cardHolderName: '',
+                  cardNumber: '',
+                ),
+              ),
+        ],
+      ),
+    ));
+  }
+
+  Widget _buttonNext(BuildContext context){
+    return Container(
+      width: double.infinity,
+      margin: EdgeInsets.symmetric(horizontal: 40, vertical: 40),
+      child: ElevatedButton(
+        onPressed: () {},
+        style: ElevatedButton.styleFrom(
+          padding: EdgeInsets.symmetric(vertical: 15)
+        ),
+        child: Text(
+          'CONTINUAR',
           style: TextStyle(
             color: Colors.black
           ),
-        )
-      ),
-      body: const Center(
-        child: Text('Pagos'),
+        ),
       ),
     );
   }
 
-  Widget _buttonNext(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      height: 50,
-      margin:const EdgeInsets.symmetric(horizontal: 20, vertical: 40),
-      child: ElevatedButton(
-          onPressed: () => paymentController.createOrder(),
-          style: ElevatedButton.styleFrom(
-              padding:const EdgeInsets.symmetric(vertical: 15)),
-          child:const Text(
-            'Continuar',
-            style: TextStyle(
-                color: Colors.black, fontWeight: FontWeight.bold, fontSize: 15),
-          )),
-    );
-  }
 }
